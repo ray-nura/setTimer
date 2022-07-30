9bd9f75e18398bb65e16068bf0d42533e00e035e
ray-nura <raynura182@gmail.com>
2022-07-30 14:29:07 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
+const intervalInput = document.querySelector("#interval");
+let intervalSecond = 500;
+let intervalID;
+
+function formatTime(date) {
+    const hours12 = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds()
+    const isAm = date.getHours() < 12;
+
+    return `${hours12.toString().padStart(2, "0")} :
+    ${minutes.toString().padStart(2, "0")} :
+    ${seconds.toString().padStart(2, "0")}
+     ${isAm ? "AM" : "PM"}`;
+}
+
+function formatDate(date) {
+    const DAYS = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    const MONTHS = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
+    return `${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]
+        } ${date.getDate()} ${date.getFullYear()}`;
+}
+
+function start() {
+    intervalID = setInterval(() => {
+        const now = new Date();
+
+        timeElement.textContent = formatTime(now);
+        dateElement.textContent = formatDate(now);
+    }, intervalSecond);
+};
+
+intervalInput.addEventListener("change", (e) => {
+    clearInterval(intervalID);
+    intervalSecond = +e.target.value * 1000;
+    start()
+    console.log(intervalSecond)
+    intervalInput.value = '';
+})
+start()
